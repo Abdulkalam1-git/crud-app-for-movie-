f333f8476d64ae15fa1a169cf0518f08cd903068
Abdulkalam.S <142289622+Abdulkalam1-git@users.noreply.github.com>
2024-08-19 12:58:11 +0530

    Update MovieController.java

--- Demo0/src/main/java/com/example/demo0/Controller/MovieController.java
+++ Demo0/src/main/java/com/example/demo0/Controller/MovieController.java
@@ -2,9 +2,11 @@ package com.example.demo0.Controller;
 
 
 import com.example.demo0.Model.MovieModel;
+import com.example.demo0.Model.MovieResponse;
 import com.example.demo0.Repository.MovieRepo;
 import com.example.demo0.Service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +18,46 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-
     @PostMapping("/post")
-    public MovieModel post(@RequestBody MovieModel movieModel) {
-        return movieService.AddMovie(movieModel);
+    public ResponseEntity<MovieResponse<MovieModel>> AddMovie(@RequestBody MovieModel movieModel) {
+        MovieResponse<MovieModel> response = movieService.addMovie(movieModel);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/get")
-    public List<MovieModel> getAllMovies() {
-        return movieService.getAllMovies();
+    public ResponseEntity<MovieResponse<List<MovieModel>>> getMovies() {
+        MovieResponse<List<MovieModel>> response = movieService.getMovie();
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/get/id/{id}")
+    public ResponseEntity<MovieResponse<MovieModel>> getMovie( @PathVariable Long id  ) {
+        MovieResponse<MovieModel> response = movieService.getMovieById(id);
+        return  ResponseEntity.ok(response);
+    }
+    @GetMapping("/get/title/{title}")
+    public ResponseEntity<MovieResponse<List<MovieModel>>> getMovie(@PathVariable String title) {
+        MovieResponse<List<MovieModel>> response = movieService.getByTitle(title);
+        return ResponseEntity.ok(response);
     }
 
 
     @PutMapping("/Put/{id}")
-    public MovieModel put(@PathVariable Long id, @RequestBody MovieModel movieModel) {
-        return movieService.updateMovie(id, movieModel);
+    public ResponseEntity<MovieResponse<MovieModel>> updateMovie(@PathVariable Long id, @RequestBody MovieModel movieModel) {
+        MovieResponse<MovieModel> response = movieService.updateMovie(id, movieModel);
+        return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("delete/{id}")
-    public void delete(@PathVariable Long id) {
-        movieService.deleteMovieModelById(id);
+//    @DeleteMapping("delete/{id}")
+//    public ResponseEntity<Void> delete(@PathVariable Long id) {
+//        movieService.deleteMovieModelById(id);
+//        return ResponseEntity.ok().build();
+//
+//
+//    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<MovieResponse<Void>> delete(@PathVariable Long id) {
+        MovieResponse<Void> response = movieService.deleteMovieModelById(id);
+        return ResponseEntity.ok(response);
     }
-
 
 }
